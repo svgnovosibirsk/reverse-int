63eb445e12c4becdf981159f5aeabb511009cace
Sergey <a1234@1234s-mbp.local>
2023-01-14 18:44:01 +0300

    feat: implement function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    const nInt = n.toString()
+    const nIntSplit = nInt.split('');
+    const nIntReversed = nIntSplit.reverse();
+    const nIntReversedJoined = nIntReversed.join('');
+    const answer = parseInt(nIntReversedJoined);
+    return answer;
 }
